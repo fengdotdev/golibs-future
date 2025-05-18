b1a58914bcd690a5de8c95162de42bd5e322f03a
feng.dev <feng@vitruvio.cl>
2025-05-17 23:24:54 -0400

    ....

--- async/webres/constructor.go
+++ async/webres/constructor.go
@@ -81,3 +81,38 @@ func NewWebResourceComplete(url string, data []byte, err error) *WebResource {
 		ctxCancel:       nil,
 	}
 }
+
+func NewWebResourceCompleteWithTime(data []byte, delay time.Duration) *WebResource {
+	id := helpers.GenerateIdentifier("")
+	channel := make(chan []byte, 1)
+	errorChannel := make(chan error, 1)
+	completeChannel := make(chan bool, 1)
+
+	defer func() {
+		go func() {
+			// stop for a while
+			time.Sleep(delay)
+
+			channel <- data
+			errorChannel <- nil
+			completeChannel <- true
+
+			close(channel)
+			close(errorChannel)
+			close(completeChannel)
+		}()
+	}()
+
+	return &WebResource{
+		id:              id,
+		timeout:         delay,
+		channel:         channel,
+		errorChannel:    errorChannel,
+		completeChannel: completeChannel,
+		cached:          data,
+		chachedErr:      nil,
+		isCompleted:     true,
+		ctxTimeout:      nil,
+		ctxCancel:       nil,
+	}
+}
--- async/webres/webresource_test.go
+++ async/webres/webresource_test.go
@@ -9,10 +9,12 @@ import (
 	"github.com/fengdotdev/golibs-testing/assert"
 )
 
+var mockData = []byte("mock data")
+
 func TestWebResource(t *testing.T) {
 
 	t.Run("", func(t *testing.T) {
-		wr := webres.NewWebResource("http://example.com")
+		wr := webres.NewWebResourceCompleteWithTime(mockData, 2*time.Second)
 		assert.NotNil(t, wr)
 
 		fmt.Println(wr)
@@ -23,5 +25,17 @@ func TestWebResource(t *testing.T) {
 
 		// sleep for 3 seconds to allow the async operation to complete
 		time.Sleep(3 * time.Second)
+	})
+
+	t.Run("await", func(t *testing.T) {
+		wr := webres.NewWebResourceCompleteWithTime(mockData, 2*time.Second)
+		assert.NotNil(t, wr)
+
+		fmt.Println(wr)
+
+		data, err := wr.Await()
+		assert.Nil(t, err)
+		assert.NotNil(t, data)
+		fmt.Println("Await:", string(data))
 	})
 }
